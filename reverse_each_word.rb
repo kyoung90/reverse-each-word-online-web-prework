6cfa7d1fcc4e2edb025a4656b96adff34f4e6477
Kenneth Young Castro <kenneth.young90@gmail.com>
2018-10-11 15:49:30 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,6 @@
 def reverse_each_word(str)
   arr = str.split(" ")
-  arr.each_with_index do |ele, index|
-    arr[index] = ele.reverse
+  arr.collect do |ele|
+    ele.reverse!
   end 
-  return arr.join(" ")
 end 
